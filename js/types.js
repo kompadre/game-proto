e40878696dc968d10442e998d9b5d7bca21c3522
Alexey Serikov <kompadre@gmail.com>
2019-11-02 23:15:59 +0100

    New fields

---
+++ js/types.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.gameproto.v1.Types');
+
+/**
+ * @enum {number}
+ */
+proto.gameproto.v1.Types = {
+  SYSTEM: 0,
+  MAP: 1,
+  CHAT: 2,
+  OTHER: 99
+};
+
